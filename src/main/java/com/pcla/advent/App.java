a3fffc495f9d538b321801d4efd21cc0cd29458b
Pierre <pierre.clainchard@kanoma.fr>
2022-12-01 17:29:24 +0100

    Day 1 input 2

--- src/main/java/com/pcla/advent/App.java
+++ src/main/java/com/pcla/advent/App.java
@@ -7,7 +7,8 @@ public class App {
         
         File input1 = new File("src/main/resources/01/1.txt");
         Simulator simulator = new Simulator(input1);
-        simulator.day1ParseInput1();
+        simulator.day1ParseInput1
+        ();
         
     }
 }
--- src/main/java/com/pcla/advent/Simulator.java
+++ src/main/java/com/pcla/advent/Simulator.java
@@ -3,6 +3,11 @@ package com.pcla.advent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class Simulator {
@@ -13,10 +18,11 @@ public class Simulator {
 		this.file = file;
 	}
 	
-	public int day1ParseInput1() {
+	public void day1ParseInput1() {
 		BufferedReader reader;
 		int score=0;
 		int tempScore=0;
+		List<Integer> calories = new ArrayList<>();
 		
 		try {
 			reader = new BufferedReader(new FileReader(file));
@@ -26,24 +32,34 @@ public class Simulator {
 				if (! line.isEmpty()){
 					tempScore += Integer.parseInt(line);
 				}else{
-					//RAZ temp et max score
-					score = Integer.max(score, tempScore);
+					//Ajout dans calories
+					calories.add(tempScore);
 					tempScore=0;
 				}
 				// read next line
 				line = reader.readLine();
 			}
-			
 			reader.close();
-
-			
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		System.out.println("Score= " + score);
-		return score;
+		//Garder les 3 meilleures calories
+		if (calories != null && !calories.isEmpty()){
+			List<Integer> listCalories = calories.stream()
+				.sorted(Comparator.reverseOrder())
+				.limit(3)
+				.collect(Collectors.toList());
+			Iterator<Integer> it = listCalories.iterator();
+			while (it.hasNext()){
+				Integer i =  it.next();
+				score += i;
+			}
+
+		}
+
+		System.out.println("Score = " + score);
+
 	}
 	
 }
