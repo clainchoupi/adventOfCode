3a79b77ee5cc20a6b2c7499109b3b67653eba3f2
Pierre <pierre.clainchard@kanoma.fr>
2022-12-03 15:18:52 +0100

    Day 3 input 1 : OK

--- src/main/java/com/pcla/advent/Simulator.java
+++ src/main/java/com/pcla/advent/Simulator.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.CollectionUtils;
 
@@ -17,7 +18,57 @@ public class Simulator {
 	public Simulator(File file) {
 		this.file = file;
 	}
+	
+	public void day3ParseInput1() {
+		BufferedReader reader;
+		int score=0;
+		List<Character> letters = new ArrayList<Character>();
+		
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String line = reader.readLine();
+			
+			while (line != null) {
+				if (! line.isEmpty()){
+					
+					String firstAlf = line.substring(0, (line.length()/2));
+					String secondAlf = line.substring(line.length()/2, line.length());
+					
+					
+					List<Character> charsFirst = firstAlf.chars().mapToObj(e -> (char)e).collect(Collectors.toList());
+					List<Character> charsSecond = secondAlf.chars().mapToObj(e -> (char)e).collect(Collectors.toList());
+					letters.add(charsFirst.stream().distinct().filter(charsSecond::contains).findFirst().orElseThrow());
+					
+				}
+				// read next line
+				line = reader.readLine();
+			}
+			reader.close();
 
+			//On transforme les lettes en int pour avoir leur valeur en ASCII
+			for(char c : letters){
+				int asciiValue = (int)c;
+				int lowerCaseAsciiValue = 96; //for lower case
+				int upperCaseAsciiValue = 64; //for upper case
+				if(asciiValue<=122 & asciiValue>=97){
+					//Les minuscules valent de 1 à 26
+					score+=(asciiValue-lowerCaseAsciiValue);
+				}else {
+					//Les majuscules valent de 27 à 52 (on rajoute 26 à la position dans l'ordre alphab)
+					score+=(asciiValue-upperCaseAsciiValue+26);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Score day3 = " + score);
+		
+	}
+	
+	//------------------------------------------------
+	
 	public void day2ParseInput1() {
 		BufferedReader reader;
 		int score=0;
@@ -36,7 +87,7 @@ public class Simulator {
 				int rock = 1;
 				int paper = 2;
 				int scissors = 3;
-
+				
 				if ("A".equalsIgnoreCase(opponent)){
 					if("X".equalsIgnoreCase(me)){score += lost + scissors;}
 					if("Y".equalsIgnoreCase(me)){score += draw + rock;}
@@ -59,12 +110,12 @@ public class Simulator {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-
+		
+		
 		System.out.println("Score day2 = " + score);
-
+		
 	}
-
+	
 	//------------------------------------------------
 	
 	public void day1ParseInput1() {
@@ -92,17 +143,17 @@ public class Simulator {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
+		
 		//Garder les 3 meilleures calories
 		if (CollectionUtils.isNotEmpty(calories)){
 			score = calories.stream()
-				.sorted(Comparator.reverseOrder())
-				.limit(3)
-				.reduce(0, (a, b) -> a + b);
+			.sorted(Comparator.reverseOrder())
+			.limit(3)
+			.reduce(0, (a, b) -> a + b);
 		}
-
+		
 		System.out.println("Score day1 = " + score);
-
+		
 	}
 	
 }
