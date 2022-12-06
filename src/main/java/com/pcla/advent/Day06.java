3057f2c1a72506f4ba0052fe5651fd7e9804c9f2
Pierre <pierre.clainchard@kanoma.fr>
2022-12-06 15:00:05 +0100

    Day 6 Input 1&2 : OK

--- src/main/java/com/pcla/advent/Day06.java
+++ src/main/java/com/pcla/advent/Day06.java
@@ -1,8 +1,7 @@
 package com.pcla.advent;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Day06 {
@@ -10,74 +9,52 @@ public class Day06 {
         File input1 = new File("src/main/resources/DAY/06.txt");
         day6ParseInput1(input1);
     }
-
+    
     private static void day6ParseInput1(File file) {
-        int score=0;
-        HashMap<Integer, String> piles = new HashMap();
+        int position=0;
+        int result=0;
         
         try {
             Scanner reader = new Scanner(file);
             String line = reader.nextLine();
-            boolean firstBloc = true;
             
-            while (line != null) {
-                if (firstBloc){
-                    if (line.isEmpty() || line.startsWith(" 1")){
-                        //fin du premier bloc
-                        firstBloc = false;
-                        line = reader.nextLine();
-                    }else{
-                        //Lecture du premier bloc
-                        String[] inputLine = line.split("(?<=\\G.{4})");
-                        for(int i=0; i< inputLine.length; i++){
-                            if(piles.get(i) == null){
-                                piles.put(i, "");
-                            }
-                            
-                            String letter = inputLine[i].split("")[1];
-                            if (letter != null && !letter.equalsIgnoreCase(" ")){
-                                String result = letter + piles.get(i);
-                                piles.put(i, result);
-                            }
-                        }
-                        
-                    }
-                    
-                }else{
-                    //Lecture des mouvements
-                    String[] moves = line.split(" ");
-                    Integer nbMove = Integer.parseInt(moves[1]);
-                    Integer fromStack = Integer.parseInt(moves[3])-1;
-                    Integer toStack = Integer.parseInt(moves[5])-1;
-                    
-                    //Recupère la lettre a deplacer
-                    String lettersToMove = piles.get(fromStack).substring(piles.get(fromStack).length() - nbMove);
-                    
-                    //Supprime les lettres de From
-                    piles.put(fromStack, piles.get(fromStack).substring(0, piles.get(fromStack).length() - nbMove));
-                    
-                    //Ajoute la lettre dans To
-                    piles.put(toStack, piles.get(toStack)+lettersToMove);
-                    
+            // Iterate over the characters in the string
+            for (char c : line.toCharArray()) {
+                HashSet<Character> tempSet = new HashSet<Character>();
+                tempSet.add(c);
+                tempSet.add(line.charAt(position +1));
+                tempSet.add(line.charAt(position +2));
+                tempSet.add(line.charAt(position +3));
+                tempSet.add(line.charAt(position +4));
+                tempSet.add(line.charAt(position +5));
+                tempSet.add(line.charAt(position +6));
+                tempSet.add(line.charAt(position +7));
+                tempSet.add(line.charAt(position +8));
+                tempSet.add(line.charAt(position +9));
+                tempSet.add(line.charAt(position +10));
+                tempSet.add(line.charAt(position +11));
+                tempSet.add(line.charAt(position +12));
+                tempSet.add(line.charAt(position +13));
+                // Check if the four most recently received characters are all different
+                if (tempSet.size() == 14){
+                    //Found
+                    result=position;
+                    break;
                 }
                 
-                line = reader.nextLine();
+                // Increment pos by 1
+                position++;
             }
+            
+            // After the loop has finished, the first position where the four most recently received characters are all different will be stored in result
             reader.close();
             
         } catch (Exception e) {
             e.printStackTrace();
         }
+        result+=14;
         
-        //Print result
-        String resultStack = "";
-        Iterator it = piles.keySet().iterator();
-        while (it.hasNext()){
-            Integer key = (Integer) it.next();
-            resultStack += piles.get(key).substring(piles.get(key).length() - 1);
-        }
-        
-        System.out.println("Score day6 part1 = " + resultStack);
+        System.out.println("Score day6 part1 = " + result);
     }
     
 }
